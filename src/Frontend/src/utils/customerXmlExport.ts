41c4d31c6340b0fea4e0536c95f4f4ddf0bcd476
lechabisultanov-web <lecha.bisultanov@gmail.com>
2026-03-19 16:14:12 +0000

    Added XML export helper for customer list

---
+++ src/Frontend/src/utils/customerXmlExport.ts
@@ -0,0 +1,50 @@
+export interface CustomerXmlItem {
+  id: number;
+  name: string;
+  address: string;
+  email: string;
+  phone: string;
+  iban: string;
+  code: string;
+  description: string;
+}
+
+const escapeXml = (value: string) =>
+  value
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+
+export const exportCustomersToXml = (customers: CustomerXmlItem[]) => {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<Customers>
+${customers
+  .map(
+    (customer) => `<Customer>
+    <Id>${customer.id}</Id>
+    <Name>${escapeXml(customer.name)}</Name>
+    <Address>${escapeXml(customer.address)}</Address>
+    <Email>${escapeXml(customer.email)}</Email>
+    <Phone>${escapeXml(customer.phone)}</Phone>
+    <Iban>${escapeXml(customer.iban)}</Iban>
+    <Code>${escapeXml(customer.code)}</Code>
+    <Description>${escapeXml(customer.description)}</Description>
+  </Customer>`
+  )
+  .join("\n")}
+</Customers>`;
+
+  const blob = new Blob([xml], { type: "application/xml" });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "customers.xml";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+  URL.revokeObjectURL(url);
+};
